3165549411d2a8f9d7141c004c64ceb452aae563
Shobha Bhagwat <shobha_bhagwat@yahoo.co.in>
2018-05-20 13:38:39 +0530

    Add files via upload

---
+++ Tweet_Analyser/config.py
@@ -0,0 +1,12 @@
+CONSUMER_KEY = 'TyfZA7lh5guchxZneVdFHVm2p'
+CONSUMER_SECRET = 'zguNBdFlr8iqp9MD1syfTkgyxxxUT3kzsB5Q9DjZlNB8xeHhg4'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+
+
+db_user = 'postgres'
+db_pwd = 'postgres﻿'
+db_host = '127.0.0.1'
+database = 'mydb'
