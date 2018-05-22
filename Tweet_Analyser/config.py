47fdfb4f03763704112abf6239ee0ef6596cc0e3
Shobha Bhagwat <shobha_bhagwat@yahoo.co.in>
2018-05-22 23:39:47 +0530

    Update config.py

--- Tweet_Analyser/config.py
+++ Tweet_Analyser/config.py
@@ -1,5 +1,5 @@
-CONSUMER_KEY = 'TyfZA7lh5guchxZneVdFHVm2p'
-CONSUMER_SECRET = 'zguNBdFlr8iqp9MD1syfTkgyxxxUT3kzsB5Q9DjZlNB8xeHhg4'
+CONSUMER_KEY = 'sdbkjdhfjdnjfkad'
+CONSUMER_SECRET = 'bdkjahdhodlksjoidoijdlksjdhuheiu'
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
@@ -11,8 +11,8 @@ db_host = '127.0.0.1'
 database = 'mydb'
 
 
-path_pos = "/Users/shobha.bhagwat/Documents/Personal/Data engineering/project/Python_code/tweet_analyser/positive-words.txt"
-path_neg = "/Users/shobha.bhagwat/Documents/Personal/Data engineering/project/Python_code/tweet_analyser/negative-words.txt"
-path_inc = "/Users/shobha.bhagwat/Documents/Personal/Data engineering/project/Python_code/tweet_analyser/Incrementers.txt"
-path_dec = "/Users/shobha.bhagwat/Documents/Personal/Data engineering/project/Python_code/tweet_analyser/Decrementers.txt"
-path_inv = "/Users/shobha.bhagwat/Documents/Personal/Data engineering/project/Python_code/tweet_analyser/Inverters.txt"
+path_pos = "~/positive-words.txt"
+path_neg = "~/negative-words.txt"
+path_inc = "~/Incrementers.txt"
+path_dec = "~/Decrementers.txt"
+path_inv = "~/Inverters.txt"
